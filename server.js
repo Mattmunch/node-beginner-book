38c88bca71d16ea0abe101237d5e30203e2c0c82
Matt Munch <mattmunch94@gmail.com>
2019-12-03 15:58:45 -0800

    on page 17

---
+++ server.js
@@ -0,0 +1,13 @@
+let http = require('http');
+const start = () => {
+
+  function onRequest(req, res) {
+    console.log('Request received.');
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.write('Hello World');
+    res.end();
+  }
+  http.createServer(onRequest).listen(8888);
+  console.log('Server has started');
+};
+exports.start = start;
