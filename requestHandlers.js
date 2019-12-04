00da8a38da169df0daa0c7794362becf3146344c
Matt Munch <mattmunch94@gmail.com>
2019-12-03 16:58:39 -0800

    page 28

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,11 +1,17 @@
+let exec = require('child_process').exec;
 function start() {
-  console.log('Request handler \'start\' was called.');
-  return 'hello start';
+  console.log('Request handler `start` was called.');
+  let content = 'empty';
+    
+  exec('ls -lah', function(error, stdout, stderr) {
+    content = stdout;
+  });
+  return content;
 }
     
 function upload() {
   console.log('Request handler \'upload\' was called.');
-  return 'hello upload';
+  return 'Hello Upload';
 }
     
 exports.start = start;
