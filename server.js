24b358b7120c07fe8ff2e18c40bc0f7e5cbeebc3
Matt Munch <mattmunch94@gmail.com>
2019-12-03 16:18:35 -0800

    page 21

--- server.js
+++ server.js
@@ -3,11 +3,14 @@ let url = require('url');
 
 
 
-const start = () => {
+const start = (route) => {
 
   function onRequest(req, res) {
     let pathname = url.parse(req.url).pathname;
     console.log('Request for ' + pathname + ' received.');
+      
+    route(pathname);
+
     res.writeHead(200, { 'Content-Type': 'text/plain' });
     res.write('Hello World');
     res.end();
