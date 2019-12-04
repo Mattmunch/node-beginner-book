d7e4040e10f2618a69e1051ea7542f84b79fdf81
Matt Munch <mattmunch94@gmail.com>
2019-12-03 17:11:21 -0800

    page 30

--- server.js
+++ server.js
@@ -7,12 +7,7 @@ const start = (route, handle) => {
   function onRequest(req, res) {
     let pathname = url.parse(req.url).pathname;
     console.log('Request for ' + pathname + ' received.');
-      
-    let content = route(handle, pathname);
-
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.write(content);
-    res.end();
+    route(handle, pathname, res);
   }
   http.createServer(onRequest).listen(8888);
   console.log('Server has started');
