db5fa79203df6077dd31d27d7e56e089a968a40c
Matt Munch <mattmunch94@gmail.com>
2019-12-03 17:39:45 -0800

    stuck

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,14 +1,12 @@
 let exec = require('child_process').exec;
 function start(response) {
   console.log('Request handler `start` was called.');
-  let content = 'empty';
     
-  exec('ls -lah', function(error, stdout) {
+  exec('ls -lah', function(error, stdout, stderr) {
     response.writeHead(200, { 'Content-Type': 'text/plain' });
     response.write(stdout);
     response.end();
   });
-  return content;
 }
     
 function upload(response) {
