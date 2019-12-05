287734d5622f00d41244a1dfcfa2fe50b8256580
Matt Munch <mattmunch94@gmail.com>
2019-12-04 16:42:24 -0800

    crisis averted

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const start = (route, handle) => {
   function onRequest(req, response) {
     let pathname = url.parse(req.url).pathname;
     console.log('Request for ' + pathname + ' received.');
+    
     route(handle, pathname, response);
   }
   http.createServer(onRequest).listen(8888);
