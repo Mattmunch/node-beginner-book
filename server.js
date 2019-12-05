419d20b62671fdd7fe4792d7295f045c19a27810
Matt Munch <mattmunch94@gmail.com>
2019-12-04 17:02:24 -0800

    page 35

--- server.js
+++ server.js
@@ -5,10 +5,17 @@ let url = require('url');
 
 const start = (route, handle) => {
   function onRequest(req, response) {
+    let postData = '';
     let pathname = url.parse(req.url).pathname;
     console.log('Request for ' + pathname + ' received.');
-    
-    route(handle, pathname, response);
+    req.setEncoding('utf8');
+    req.addListener('data', function(postDataChunk) {
+      postData += postDataChunk;
+      console.log('Received POST data chunk' + postDataChunk + '.');
+    });
+    req.addListener('end', function() {
+      route(handle, pathname, response, postData);
+    });
   }
   http.createServer(onRequest).listen(8888);
   console.log('Server has started');
